ff5d067dec33341c2b020f5af547fcb6fec73d8f
Noor Shaikh <developernoorshaikh00@gmail.com>
2024-12-21 21:44:43 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+//BMI
+var WeightInKg = 70;
+var heightInMeters = 1.75;
+var bmi = WeightInKg / (heightInMeters * heightInMeters);
+console.log("your bmi is ".concat(bmi));
---
+++ main.ts
@@ -0,0 +1,5 @@
+//BMI
+ let WeightInKg = 70;
+ let heightInMeters =1.75;
+ let bmi = WeightInKg / (heightInMeters * heightInMeters);
+ console.log(`your bmi is ${bmi}`); 
